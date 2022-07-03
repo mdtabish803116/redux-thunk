63e8327f4901dd7a4c90c77eaf10f9adc89df118
mdtabish803116 <mdtabish803116@gmail.com>
2022-07-04 02:15:04 +0530

    added modified files

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,7 +3,8 @@ import { Input , Button , Center} from '@chakra-ui/react';
 import {useDispatch , useSelector} from "react-redux";
 import { Spinner} from '@chakra-ui/react';
 import {useNavigate} from "react-router-dom";
-import {getLogin} from "./../Redux/Login/action"
+import {getLogin} from "./../Redux/Login/action";
+import Styled from "styled-components";
 
 import {
      AlertDialog,
@@ -14,15 +15,21 @@ import {
      useDisclosure 
    } from '@chakra-ui/react'
 
-
+const ErrorDiv = Styled.div`
+color : red;
+font-size : 15px;
+font-weight : bold;
+`
 
 export const Login = () =>{
      const { isOpen, onOpen, onClose } = useDisclosure();
      const cancelRef = React.useRef();
 
-
      const [password , setPassword] = React.useState('');
+     const [passwordError , setPasswordError] = React.useState(false);
+     const [mustPasswordError , setMustPasswordError] = React.useState(false);
      const [userName , setUserName] = React.useState('');
+     const [userNameError , setUserNameError] = React.useState(false);
      const dispatch = useDispatch();
      const navigate = useNavigate();
      const {loading , error , loginUser} = useSelector((state) => state.loginUser)
@@ -34,7 +41,27 @@ export const Login = () =>{
 
      const handleSubmit = (e) => {
           e.preventDefault()
-         if(password !== "" && userName !== ""){  
+          if(password === ""){
+               setPasswordError(true);
+         }else {
+              setPasswordError(false);
+         }
+  
+         if(password !== "" && password.length < 10){
+                setMustPasswordError(true);
+          }else {
+               setMustPasswordError(false);
+          }
+
+          if(userName === ""){
+               setUserNameError(true);
+        }else {
+                setUserNameError(false);
+        }
+
+
+  
+         if(password !== "" && userName !== "" && password.length >= 10){  
           onOpen()
           dispatch(getLogin(payload))
          }
@@ -86,7 +113,12 @@ export const Login = () =>{
                marginTop : "50px"
           }}>
                   <Input mt = "20px" variant = "outline" type = "text" value = {userName} onChange = {(e) => setUserName(e.target.value)} placeholder = "Enter UserName" border = "2px solid black"/> 
+                  {userNameError && <ErrorDiv>Hey , We need a userName here</ErrorDiv>}
+                
                  <Input mt = "20px" variant = "outline" type = "password" value = {password} onChange = {(e) => setPassword(e.target.value)} placeholder = "Enter Password" border = "2px solid black"/> 
+                 {mustPasswordError && <ErrorDiv>Must be 10 or more Characters</ErrorDiv>}
+                 {passwordError && <ErrorDiv>Hey , We need a password here</ErrorDiv>}
+                  
                   <Center>
                     <Button type = "submit" mt = "20px" onClick = {handleSubmit}>LOGIN</Button>
                   </Center>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -4,26 +4,40 @@ import {getRegister} from "./../Redux/Register/action";
 import {useDispatch , useSelector} from "react-redux";
 import { Spinner} from '@chakra-ui/react';
 import {useNavigate} from "react-router-dom";
+import Styled from "styled-components";
 import {
       AlertDialog,
       AlertDialogBody,
       AlertDialogFooter,
-      AlertDialogHeader,
       AlertDialogContent,
       AlertDialogOverlay,
       useDisclosure 
     } from '@chakra-ui/react'
+
+
+    const ErrorDiv = Styled.div`
+        color : red;
+        font-size : 15px;
+        font-weight : bold;
+    `
 
 export const Register = () =>{
       const { isOpen, onOpen, onClose } = useDisclosure();
       const cancelRef = React.useRef();
 
     const [name , setName] = React.useState('');
+    const [nameError , setNameError] = React.useState(false);
     const [email , setEmail] = React.useState('');
+    const [emailError , setEmailError] = React.useState(false);
     const [password , setPassword] = React.useState('');
+    const [passwordError , setPasswordError] = React.useState(false);
+    const [mustPasswordError , setMustPasswordError] = React.useState(false);
     const [userName , setUserName] = React.useState('');
+    const [userNameError , setUserNameError] = React.useState(false);
     const [mobile, setMobile] = React.useState('');
+    const [mobileError , setMobileError] = React.useState(false);
     const [desc , setDesc] = React.useState('');
+    const [descError , setDescError] = React.useState(false);
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const {loading , error , user} = useSelector((state) => state.registerUser)
@@ -59,7 +73,49 @@ export const Register = () =>{
 
 const handleSubmit = (e) => {
       e.preventDefault()
-  if(name !== "" && email !== "" && password !== "" && userName !== "" && mobile !== "" && desc !== ""){  
+      if(email === ""){
+            setEmailError(true);
+       }else {
+            setEmailError(false);
+       }
+
+       if(password === ""){
+             setPasswordError(true);
+       }else {
+            setPasswordError(false);
+       }
+
+       if(password !== "" && password.length < 10){
+              setMustPasswordError(true);
+        }else {
+             setMustPasswordError(false);
+        }
+
+       if(name === ""){
+              setNameError(true)
+       }else {
+             setNameError(false)
+       }
+
+       if(userName === ""){
+              setUserNameError(true);
+       }else {
+               setUserNameError(false);
+       }
+
+       if(mobile === ""){
+              setMobileError(true);
+       }else {
+               setMobileError(false);
+       }
+
+       if(desc === ""){
+            setDescError(true);
+     }else {
+             setDescError(false);
+     }
+
+  if(name !== "" && email !== "" && password !== "" && userName !== "" && mobile !== "" && desc !== "" && password.length >= 10){  
       dispatch(getRegister(payload))
       onOpen()
   }
@@ -111,14 +167,31 @@ if(error){
                margin :"auto",
                marginTop : "50px"
           }}>
-                 <Input mt = "20px" variant = "outline" type = "text" value = {name} onChange = {(e) => setName(e.target.value)} placeholder = "Enter Name" border = "2px solid black"/> 
-                 <Input mt = "20px" variant = "outline" type = "email" value = {email} onChange = {(e) => setEmail(e.target.value)} placeholder = "Enter Email" border = "2px solid black"/> 
-                 <Input mt = "20px" variant = "outline" type = "password" value = {password} onChange = {(e) => setPassword(e.target.value)} placeholder = "Enter Password" border = "2px solid black"/> 
-                 <Input mt = "20px" variant = "outline" type = "text" value = {userName} onChange = {(e) => setUserName(e.target.value)} placeholder = "Enter UserName" border = "2px solid black"/> 
-                 <Input mt = "20px" variant = "outline" type = "tel" value = {mobile} onChange = {(e) => setMobile(e.target.value)} placeholder = "Enter Mobile" border = "2px solid black"/> 
-                 <Input mt = "20px" variant = "outline" type = "text" value = {desc} onChange = {(e) => setDesc(e.target.value)} placeholder = "Enter Description" border = "2px solid black"/> 
+                 <Input mt = "40px" variant = "outline" type = "text" value = {name} onChange = {(e) => setName(e.target.value)} placeholder = "Enter Name" border = "2px solid black"/> 
+                 {nameError && <ErrorDiv>We need your name - it's nicer that way</ErrorDiv>}
+                 
+                 <Input mt = "40px" variant = "outline" type = "email" value = {email} onChange = {(e) => setEmail(e.target.value)} placeholder = "Enter Email" border = "2px solid black"/> 
+                 {emailError && <ErrorDiv>Oops! You need to type your Email</ErrorDiv>}
+                
+                 
+                 <Input mt = "40px" variant = "outline" type = "password" value = {password} onChange = {(e) => setPassword(e.target.value)} placeholder = "Enter Password" border = "2px solid black"/> 
+                 {mustPasswordError && <ErrorDiv>Must be 10 or more Characters</ErrorDiv>}
+                 {passwordError && <ErrorDiv>Hey , We need a password here</ErrorDiv>}
+                
+                
+                 <Input mt = "40px" variant = "outline" type = "text" value = {userName} onChange = {(e) => setUserName(e.target.value)} placeholder = "Enter UserName" border = "2px solid black"/> 
+                 {userNameError && <ErrorDiv>Hey , We need a userName here</ErrorDiv>}
+                 
+                 <Input mt = "40px" variant = "outline" type = "tel" value = {mobile} onChange = {(e) => setMobile(e.target.value)} placeholder = "Enter Mobile" border = "2px solid black"/> 
+                 {mobileError && <ErrorDiv>Hey , We need a mobile no. here</ErrorDiv>}
+                 
+                 
+                 <Input mt = "40px" variant = "outline" type = "text" value = {desc} 
+                    onChange = {(e) => setDesc(e.target.value)} placeholder = "Enter Description" border = "2px solid black"/> 
+                 {descError && <ErrorDiv>Hey , We need a userName here</ErrorDiv>}
+                 
                   <Center>
-                    <Button type = "submit" mt = "20px" onClick = {handleSubmit}>REGISTER</Button>
+                    <Button type = "submit" mt = "50px" onClick = {handleSubmit}>REGISTER</Button>
                   </Center>
                   <AlertDialog
         isOpen={isOpen}
@@ -128,9 +201,6 @@ if(error){
       >
         <AlertDialogOverlay>
           <AlertDialogContent>
-            <AlertDialogHeader fontSize='lg' fontWeight='bold'>
-              Delete Customer
-            </AlertDialogHeader>
 
             <AlertDialogBody>
                    {
